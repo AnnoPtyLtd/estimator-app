7628191fe44959d7fc145b99f443c1311684da5c
Afaq Ahmed <afaaaq85@gmail.com>
2023-10-25 11:06:01 +0500

    removed bcrypt

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors");
 const path = require('path')
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 const app = express();
 // require('dotenv').config();
 const ComponentModel = require('./models/NewComponent');
@@ -30,10 +30,10 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-UserSchema.methods.comparePassword = async function (password) {
-    const isMatch = await bcrypt.compare(password, this.password);
-    return isMatch;
-};
+// UserSchema.methods.comparePassword = async function (password) {
+//     const isMatch = await bcrypt.compare(password, this.password);
+//     return isMatch;
+// };
 
 const User = mongoose.model('users', UserSchema);
 User.createIndexes();
